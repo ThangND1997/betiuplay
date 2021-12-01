c8ad37f2f4ea7ff52a5aadd63eba7face18e3dc1
Dev-ThangNguyen <thang1997iuh@gmail.com>
2021-12-01 07:53:36 +0700

    upcode 12/1/2021

--- handleWeb/filmABC.js
+++ handleWeb/filmABC.js
@@ -232,14 +232,14 @@ const appItemsFilm = {
         }
         ,
         {
-            src: 'https://video.fhph1-1.fna.fbcdn.net/v/t42.27313-2/10000000_1295665937528370_2205191848984991247_n.mp4?_nc_cat=108&vs=c64d9e5a1de84145&_nc_vs=HBksFQAYJEdJQ1dtQUF5TnZTN1pwb0VBQTlXVnBSTmFwb2VickZxQUFBRhUAAsgBABUAGCRHSUNXbUFBalJtbjFleFVDQUZvNU5kYUxWbmdnYnJGcUFBQUYVAgLIAQBLBogScHJvZ3Jlc3NpdmVfcmVjaXBlATENc3Vic2FtcGxlX2ZwcwAQdm1hZl9lbmFibGVfbnN1YgAgbWVhc3VyZV9vcmlnaW5hbF9yZXNvbHV0aW9uX3NzaW0AKGNvbXB1dGVfc3NpbV9vbmx5X2F0X29yaWdpbmFsX3Jlc29sdXRpb24AEWRpc2FibGVfcG9zdF9wdnFzABUAJQAcAAAmyq%2FiosW4kgMVkE4oAkMzGAt2dHNfcHJldmlldxwXQL0uEeuFHrgYJ2Rhc2hfZ2VuM2Jhc2ljXzVzZWNnb3BfaHEyX2ZyYWdfMl92aWRlbxIAGBh2aWRlb3MudnRzLmNhbGxiYWNrLnByb2QZHBUAFaC2BAAoElZJREVPX1ZJRVdfUkVRVUVTVBsNiBVvZW1fdGFyZ2V0X2VuY29kZV90YWcGb2VwX2hkE29lbV9yZXF1ZXN0X3RpbWVfbXMNMTYzODExNzM2ODIyOQxvZW1fY2ZnX3J1bGUHdW5tdXRlZBNvZW1fcm9pX3JlYWNoX2NvdW50ATAMb2VtX3JvaV9ub3RlC3Byb2dyZXNzaXZlEW9lbV9yb2lfdXNlcl90aWVyAB5vZW1fcm9pX3ByZWRpY3RlZF93YXRjaF90aW1lX3MBMBZvZW1fcm9pX3JlY2lwZV9iZW5lZml0BTAuMDAwJW9lbV9yb2lfc3RhdGljX2JlbmVmaXRfY29zdF9ldmFsdWF0b3ILcHJvZ3Jlc3NpdmUMb2VtX3ZpZGVvX2lkDzg4NDk3ODczNTU1MzU1MBJvZW1fdmlkZW9fYXNzZXRfaWQPODg0OTc4NzI4ODg2ODg0FW9lbV92aWRlb19yZXNvdXJjZV9pZA84ODQ5Nzg3MTg4ODY4ODUcb2VtX3NvdXJjZV92aWRlb19lbmNvZGluZ19pZBAxMDE5NDc2ODc4NjEyNzQyJQIcHBwV8OYXGwFVAAIbAVUAAhwVAgAAABaAurcDACXEARsHiAFzBDE5NTYCY2QKMjAyMS0xMS0yMgNyY2IBMANhcHASU3R1ZGlvIHPDoW5nIHThuqFvAmN0GUNPTlRBSU5FRF9QT1NUX0FUVEFDSE1FTlQTb3JpZ2luYWxfZHVyYXRpb25fcwc3NDcwLjA1AnRzD29lcF9wcm9ncmVzc2l2ZQA%3D&ccb=1-5&_nc_sid=41a7d5&efg=eyJ2ZW5jb2RlX3RhZyI6Im9lcF9oZCIsInNvdXJjZV9zZWdtZW50X2hhbmRsZSI6bnVsbH0%3D&_nc_ohc=Y1-sX83sGVoAX-NM2Gq&_nc_ht=video-frt3-1.xx&oh=c59a6f5a24e00440f4e0dc5bba49001a&oe=61A59A76&_nc_rid=17b5e094dd8541f',
+            src: 'https://video.fhph1-1.fna.fbcdn.net/v/t42.27313-2/10000000_1295665937528370_2205191848984991247_n.mp4?_nc_cat=108&vs=c64d9e5a1de84145&_nc_vs=HBksFQAYJEdJQ1dtQUF5TnZTN1pwb0VBQTlXVnBSTmFwb2VickZxQUFBRhUAAsgBABUAGCRHSUNXbUFBalJtbjFleFVDQUZvNU5kYUxWbmdnYnJGcUFBQUYVAgLIAQBLBogScHJvZ3Jlc3NpdmVfcmVjaXBlATENc3Vic2FtcGxlX2ZwcwAQdm1hZl9lbmFibGVfbnN1YgAgbWVhc3VyZV9vcmlnaW5hbF9yZXNvbHV0aW9uX3NzaW0AKGNvbXB1dGVfc3NpbV9vbmx5X2F0X29yaWdpbmFsX3Jlc29sdXRpb24AEWRpc2FibGVfcG9zdF9wdnFzABUAJQAcAAAmyq%2FiosW4kgMVkE4oAkMzGAt2dHNfcHJldmlldxwXQL0uEeuFHrgYJ2Rhc2hfZ2VuM2Jhc2ljXzVzZWNnb3BfaHEyX2ZyYWdfMl92aWRlbxIAGBh2aWRlb3MudnRzLmNhbGxiYWNrLnByb2QZHBUAFaC2BAAoElZJREVPX1ZJRVdfUkVRVUVTVBsNiBVvZW1fdGFyZ2V0X2VuY29kZV90YWcGb2VwX2hkE29lbV9yZXF1ZXN0X3RpbWVfbXMNMTYzODI4MDI0MTE0OQxvZW1fY2ZnX3J1bGUHdW5tdXRlZBNvZW1fcm9pX3JlYWNoX2NvdW50ATAMb2VtX3JvaV9ub3RlC3Byb2dyZXNzaXZlEW9lbV9yb2lfdXNlcl90aWVyAB5vZW1fcm9pX3ByZWRpY3RlZF93YXRjaF90aW1lX3MBMBZvZW1fcm9pX3JlY2lwZV9iZW5lZml0BTAuMDAwJW9lbV9yb2lfc3RhdGljX2JlbmVmaXRfY29zdF9ldmFsdWF0b3ILcHJvZ3Jlc3NpdmUMb2VtX3ZpZGVvX2lkDzg4NDk3ODczNTU1MzU1MBJvZW1fdmlkZW9fYXNzZXRfaWQPODg0OTc4NzI4ODg2ODg0FW9lbV92aWRlb19yZXNvdXJjZV9pZA84ODQ5Nzg3MTg4ODY4ODUcb2VtX3NvdXJjZV92aWRlb19lbmNvZGluZ19pZBAxMDE5NDc2ODc4NjEyNzQyJQIcHBwV8OYXGwFVAAIbAVUAAhwVAgAAABaAurcDACXEARsHiAFzBDE5NTYCY2QKMjAyMS0xMS0yMgNyY2IBMANhcHASU3R1ZGlvIHPDoW5nIHThuqFvAmN0GUNPTlRBSU5FRF9QT1NUX0FUVEFDSE1FTlQTb3JpZ2luYWxfZHVyYXRpb25fcwc3NDcwLjA1AnRzD29lcF9wcm9ncmVzc2l2ZQA%3D&ccb=1-5&_nc_sid=41a7d5&efg=eyJ2ZW5jb2RlX3RhZyI6Im9lcF9oZCIsInNvdXJjZV9zZWdtZW50X2hhbmRsZSI6bnVsbH0%3D&_nc_ohc=fB5as7jpbPsAX_XiR8_&_nc_ht=video-frt3-1.xx&oh=9aed2e339a4b2b42fc043df399756ef1&oe=61A794B6&_nc_rid=95de40f704d8410',
             path: 'https://javdoe.sh/media/videos/tmb/000/107/970/1.jpg',
             name: 'Me ke dam dang marina shiraishi '
             
         }
         ,
         {
-            src: 'https://redirector.googlevideo.com/videoplayback?expire=1638222709&ei=FfekYbrmFs-vyQXUt6iIBQ&ip=65.21.105.248&id=o-AA4OlEx3_HWDqM4w-Uca9diPjHCwGahvypNHy4BrFTwY&itag=22&source=youtube&requiressl=yes&mh=_o&mm=32&mn=sn-5goeen7d&ms=su&mv=u&mvi=1&pl=25&sc=yes&vprv=1&prv=1&mime=video%2Fmp4&cnr=14&ratebypass=yes&dur=7120.143&lmt=1632128076478007&mt=1638200272&fexp=24001373%2C24007246&txp=6211224&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cvprv%2Cprv%2Cmime%2Ccnr%2Cratebypass%2Cdur%2Clmt&sig=AOq0QJ8wRQIgOgtg5lZS-j0NMKXSe2HhCy-UcR03Cw-riop3wKjOIigCIQC96j3cBgYdSjKEyqbbYakgZqbSEnxat6LxDLHW-iRhcw%3D%3D&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Csc&lsig=AG3C_xAwRQIgWS-xMkc1onJzwLFOegnIYSHAq-UBqBdbMbEhrHYch7UCIQC5sYyUubnFEKeBpc6iwi5Bxw4kP3vbA_I_OcsR9WSWDg%3D%3D',
+            src: 'https://video.fhph1-1.fna.fbcdn.net/v/t42.1790-2/10000000_423830532515801_5963359634454360916_n.mp4?_nc_cat=106&ccb=1-5&_nc_sid=985c63&efg=eyJybHIiOjMzNCwicmxhIjo0MDk2LCJ2ZW5jb2RlX3RhZyI6InN2ZV9zZCJ9&_nc_ohc=ztn9tO-BLGIAX84_7C-&rl=334&vabr=186&_nc_ht=video-frt3-1.xx&oh=a8291f618c92f6e129b7802a0a15a015&oe=61A7ED18',
             path: 'https://static.sextb.net/poster/adn-018.jpg',
             name: 'Thác loạn với cậu em hư hỏng ai sayama'
             
@@ -253,57 +253,57 @@ const appItemsFilm = {
         }
         ,
         {
-            src: 'https://video.fhph1-1.fna.fbcdn.net/v/t42.1790-2/10000000_408268364312423_9200881441918919966_n.mp4?_nc_cat=103&ccb=1-5&_nc_sid=985c63&efg=eyJybHIiOjM2MSwicmxhIjo0MDk2LCJ2ZW5jb2RlX3RhZyI6InN2ZV9zZCJ9&_nc_ohc=yyyzGFR6sFwAX-Xn-TL&rl=361&vabr=201&_nc_ht=video-frt3-2.xx&oh=04f4ec922d36420eba5aa3422d31b7e6&oe=61A59111',
+            src: 'https://video.fhph1-1.fna.fbcdn.net/v/t42.1790-2/10000000_408268364312423_9200881441918919966_n.mp4?_nc_cat=103&ccb=1-5&_nc_sid=985c63&efg=eyJybHIiOjM2MSwicmxhIjo0MDk2LCJ2ZW5jb2RlX3RhZyI6InN2ZV9zZCJ9&_nc_ohc=yyyzGFR6sFwAX-Xn-TL&rl=361&vabr=201&_nc_ht=video-frt3-2.xx&oh=7dcda44c68ba92d24e7d0490c79f96be&oe=61A78B51',
             path: 'https://xjapanese.com/pornpics/aka-asuka/oae00212/aka-asuka-0-300.jpg',
             name: 'Em đồng nghiệp Aka Asuka'
             
         }
         ,
         {
-            src: 'https://redirector.googlevideo.com/videoplayback?expire=1638218659&ei=Q-ekYcDxFdXtyQXxxJ0w&ip=65.21.105.248&id=o-ADr8-k8k4A8rmfYuivqQRytA7RqLMbWIP3Q8_hmP_nQm&itag=22&source=youtube&requiressl=yes&mh=eZ&mm=32&mn=sn-5goeen7d&ms=su&mv=u&mvi=1&pl=25&sc=yes&vprv=1&prv=1&mime=video%2Fmp4&cnr=14&ratebypass=yes&dur=7008.734&lmt=1637770574013080&mt=1638196588&fexp=24001373%2C24007246&txp=6211224&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cvprv%2Cprv%2Cmime%2Ccnr%2Cratebypass%2Cdur%2Clmt&sig=AOq0QJ8wRQIgXnSbpsKN41nl-j7eqZ2jK0qHnVrKpUpf1Kna_pUYpIkCIQCkhW62ODBFMI18Z-1OZxGuNsoTqSYT0aPb_yneQR86eg%3D%3D&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Csc&lsig=AG3C_xAwRgIhAJt1a6My8IMXAdltffiSfCCZv1dbO-SFujdyH9ZmAlJoAiEAj4gkA5NOm5uumf8ZXaxU-Ior6k4YDSBapVGsKrHhRAc%3D',
+            src: 'https://video.fhph1-1.fna.fbcdn.net/v/t66.36240-6/10000000_4567755046671632_1523789648021009772_n.mp4?_nc_cat=108&ccb=1-5&_nc_sid=985c63&efg=eyJybHIiOjE1MDQsInJsYSI6NDA5NiwidmVuY29kZV90YWciOiJvZXBfaGQifQ%3D%3D&_nc_ohc=Zt9rZSboR9gAX__23Us&rl=1504&vabr=1003&_nc_ht=scontent-frt3-1.xx&oh=2e0540f086bf90a5aa428ab9371af3e3&oe=61AB3F8A',
             path: 'https://i2.wp.com/xopenload.pw/wp-content/uploads/2021/03/VEMA-097.jpg?fit=375%2C541&ssl=1',
             name: 'Em giúp việc xinh đẹp'
             
         }
         ,
-        // {
-        //     src: './assets/trailer/Chú Khủng Long Tốt Bụng - Trailer Chính Thức.mp4',
-        //     path: './assets/img/poster-film/end10.jpg',
-        //     name: 'Chú Khủng Long Tốt Bụng'
-            
-        // }
-        // ,
-        // {
-        //     src: './assets/trailer/A DOG NAMED PALMA ★ Russian movie trailer 2021.mp4',
-        //     path: './assets/img/poster-film/end11.jpg',
-        //     name: 'Chú Chó PalMa'
-            
-        // }
-        // ,
-        // {
-        //     src: './assets/trailer/The Call Of The Wild- Tiếng Gọi Nơi Hoang Dã - Legend.mp4',
-        //     path: './assets/img/poster-film/end15.jpg',
-        //     name: 'Tiếng Gọi Nơi Hoang Dã'
-            
-        // }
-        // ,
-        // {
-        //     src: './assets/trailer/The Call Of The Wild- Tiếng Gọi Nơi Hoang Dã - Legend.mp4',
-        //     path: './assets/img/poster-film/end13.jpg',
-        //     name: 'Tiếng Gọi Nơi Hoang Dã'
-            
-        // }
-        // ,
-        // {
-        //     src: './assets/trailer/Walking With Dinosaurs - Dạo Bước Cùng Khủng Long - Trailer E.mp4',
-        //     path: './assets/img/poster-film/end14.jpg',
-        //     name: 'Dạo Bước Khủng Long'
-            
-        // }
-        // ,
         {
-            src: 'https://redirector.googlevideo.com/videoplayback?expire=1638222712&ei=GPekYfnPDIiwyQX_2rOIDw&ip=65.21.105.248&id=o-AFPT7xnDUkBLOGkUwcZlnyQN99Wde1Sn2LjpIHX5mcAK&itag=22&source=youtube&requiressl=yes&mh=EY&mm=32&mn=sn-5goeen7r&ms=su&mv=u&mvi=1&pl=25&sc=yes&vprv=1&prv=1&mime=video%2Fmp4&cnr=14&ratebypass=yes&dur=10732.100&lmt=1637770575319010&mt=1638200272&fexp=24001373%2C24007246&txp=6211224&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cvprv%2Cprv%2Cmime%2Ccnr%2Cratebypass%2Cdur%2Clmt&sig=AOq0QJ8wRQIhANglW3jBP5RMXhakV4B43tbwjZ903vIoCAnTUgufxgGSAiBaMCcXaj3wkHEzhbrmKIfc86bv7R6XFSO-1wnwK2vVzA%3D%3D&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Csc&lsig=AG3C_xAwRQIhAPQJhR6NW0rZrohRu8eEwUAYnXpTx8Oi5H1YybYZ-RgDAiAo0cOHggVnSWQRnUXmDl-Zw89TJ2iBTWLAXgEK2aU9Nw%3D%3D',
-            path: 'https://javcl.com/wp-content/uploads/2020/06/patsupatsu-clothing-big-breasts-we-are-reverse-sex.jpg',
+            src: 'https://video.fhph1-1.fna.fbcdn.net/v/t66.36240-6/10000000_125808009892808_3582196089384586831_n.mp4?_nc_cat=1&ccb=1-5&_nc_sid=985c63&efg=eyJybHIiOjIwMjAsInJsYSI6NDA5NiwidmVuY29kZV90YWciOiJvZXBfaGQifQ%3D%3D&_nc_ohc=yGnxYA2tN3AAX86Cbmb&rl=2020&vabr=1347&_nc_ht=scontent-frx5-1.xx&oh=312f967735241263c0089c8123bbc9b4&oe=61AB9C9D',
+            path: 'https://danviet.mediacdn.vn/upload/2-2018/images/2018-06-23/7-1529742628-width650height858.jpg',
+            name: 'Em gai xinh dep tai nha'
+            
+        }
+        ,
+        {
+            src: 'https://video.fhph1-1.fna.fbcdn.net/v/t42.27313-2/10000000_858703231545078_3486579932051737661_n.mp4?_nc_cat=1&vs=19dac2718b1ba73e&_nc_vs=HBksFQAYJEdJQ1dtQUQyYmhWdC1Bd0RBRDBNODdyNTBXSXdickZxQUFBRhUAAsgBABUAGCRHSUNXbUFBeFU2Wk10bjRBQU9tV1BMdFQ1UEZQYnJGcUFBQUYVAgLIAQBLBogScHJvZ3Jlc3NpdmVfcmVjaXBlATENc3Vic2FtcGxlX2ZwcwAQdm1hZl9lbmFibGVfbnN1YgAgbWVhc3VyZV9vcmlnaW5hbF9yZXNvbHV0aW9uX3NzaW0AKGNvbXB1dGVfc3NpbV9vbmx5X2F0X29yaWdpbmFsX3Jlc29sdXRpb24AEWRpc2FibGVfcG9zdF9wdnFzABUAJQAcAAAmwKGAk9eSwAEVkE4oAkMzGAt2dHNfcHJldmlldxwXQJ3xUeuFHrgYJ2Rhc2hfZ2VuM2Jhc2ljXzVzZWNnb3BfaHEyX2ZyYWdfMl92aWRlbxIAGBh2aWRlb3MudnRzLmNhbGxiYWNrLnByb2QZHBUAFaC2BAAoElZJREVPX1ZJRVdfUkVRVUVTVBsNiBVvZW1fdGFyZ2V0X2VuY29kZV90YWcGb2VwX2hkE29lbV9yZXF1ZXN0X3RpbWVfbXMNMTYzODI4MDIwOTU2NQxvZW1fY2ZnX3J1bGUHdW5tdXRlZBNvZW1fcm9pX3JlYWNoX2NvdW50ATAMb2VtX3JvaV9ub3RlC3Byb2dyZXNzaXZlEW9lbV9yb2lfdXNlcl90aWVyAB5vZW1fcm9pX3ByZWRpY3RlZF93YXRjaF90aW1lX3MBMBZvZW1fcm9pX3JlY2lwZV9iZW5lZml0BTAuMDAwJW9lbV9yb2lfc3RhdGljX2JlbmVmaXRfY29zdF9ldmFsdWF0b3ILcHJvZ3Jlc3NpdmUMb2VtX3ZpZGVvX2lkDzQyMjUzMzQwOTU3ODcxORJvZW1fdmlkZW9fYXNzZXRfaWQPNDIyNTMzNDAyOTEyMDUzFW9lbV92aWRlb19yZXNvdXJjZV9pZA80MjI1MzMzOTk1Nzg3MjAcb2VtX3NvdXJjZV92aWRlb19lbmNvZGluZ19pZA81OTg2MDIyMTE0NzUyNTYlAhwcHBXw5hcbAVUAAhsBVQACHBUCAAAAFoC6twMAJcQBGweIAXMDMTMzAmNkCjIwMjEtMTEtMTkDcmNiATADYXBwElN0dWRpbyBzw6FuZyB04bqhbwJjdBlDT05UQUlORURfUE9TVF9BVFRBQ0hNRU5UE29yaWdpbmFsX2R1cmF0aW9uX3MIMTkxNi4zMTgCdHMPb2VwX3Byb2dyZXNzaXZlAA%3D%3D&ccb=1-5&_nc_sid=41a7d5&efg=eyJ2ZW5jb2RlX3RhZyI6Im9lcF9oZCIsInNvdXJjZV9zZWdtZW50X2hhbmRsZSI6bnVsbH0%3D&_nc_ohc=pS5JPrZATd0AX91KjoW&_nc_ht=video-frx5-1.xx&oh=90256e944030e27bccd6775785469cd7&oe=61A7B29C&_nc_rid=af0abe28052749e',
+            path: 'https://thongcaucongnghet.info/uploads/tieusumelodymarks/melody-marks-10.jpg',
+            name: 'Chuyen du lich cung em'
+            
+        }
+        ,
+        {
+            src: 'https://video.fhph1-1.fna.fbcdn.net/v/t66.36240-6/10000000_695923691811658_8548958732098580970_n.mp4?_nc_cat=107&ccb=1-5&_nc_sid=985c63&efg=eyJybHIiOjM0NzUsInJsYSI6NDA5NiwidmVuY29kZV90YWciOiJvZXBfaGQifQ%3D%3D&_nc_ohc=R0pJAmBihYgAX9bJY87&rl=3475&vabr=2317&_nc_ht=scontent-frt3-1.xx&oh=b9980b9dfc9a0265be407e416d3dfdd2&oe=61AAD00B',
+            path: 'https://api.chich.info/Uploads/Album/anh-sex-gai-chau-au%20(18).jpg',
+            name: 'Ga chich em tren pho'
+            
+        }
+        ,
+        {
+            src: 'https://video.fhph1-1.fna.fbcdn.net/v/t42.1790-2/10000000_577193870208528_6897522989478438018_n.mp4?_nc_cat=103&ccb=1-5&_nc_sid=985c63&efg=eyJybHIiOjM5NiwicmxhIjo0MDk2LCJ2ZW5jb2RlX3RhZyI6InN2ZV9zZCJ9&_nc_ohc=oKxt_l23EvgAX-oFI-q&rl=396&vabr=220&_nc_ht=video-frt3-2.xx&oh=3a594c7a3f5ef086aeed4204e11f26b7&oe=61A7BC1E',
+            path: 'https://ae01.alicdn.com/kf/Hf0d511ff452d4dbcb9c70de068057feez.jpg',
+            name: 'Chuyen du lich ngot ngao voi em Melody'
+            
+        }
+        ,
+        {
+            src: 'https://video.fhph1-1.fna.fbcdn.net/v/t66.36240-6/10000000_320528246241709_7830470949008946672_n.mp4?_nc_cat=107&ccb=1-5&_nc_sid=985c63&efg=eyJybHIiOjMyNDksInJsYSI6NDA5NiwidmVuY29kZV90YWciOiJvZXBfaGQifQ%3D%3D&_nc_ohc=HRluzIeEGLEAX8Z-yme&rl=3249&vabr=2166&_nc_ht=scontent-frt3-1.xx&oh=f8bf907fea514380d15efa3eb415e82a&oe=61ABB68D',
+            path: 'https://asset1.modelmanagement.com/mm-eyJ0Ijp7InIiOnsibCI6/IjE2MDAiLCJoIjoiMTIw/MCJ9fSwiaWQiOiJpNzUw/ODE5NyIsImYiOiJqcGci/fQ;;.jpg',
+            name: 'Cach melody nho sua xe'
+            
+        }
+        ,
+        {
+            src: 'https://video.fhph1-1.fna.fbcdn.net/v/t66.36240-6/10000000_688537985363972_5383715241904905650_n.mp4?_nc_cat=1&ccb=1-5&_nc_sid=985c63&efg=eyJybHIiOjE1MDAsInJsYSI6NDA5NiwidmVuY29kZV90YWciOiJvZXBfaGQifQ%3D%3D&_nc_ohc=7ZhoTWKaJUcAX9QSbAw&rl=1500&vabr=942&_nc_ht=scontent-frx5-1.xx&oh=dce25af7f8f1f9311e8ddab5e1cb017b&oe=61AB166E',
+            path: 'https://m.media-amazon.com/images/I/411uVC0M7lL.jpg',
             name: 'Phịch em đồng nghiệp Ai-saayama'
             
         }
